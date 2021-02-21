e55203d488d7c8d8eb16399e07153688e78d0993
Ashar Khan <ashar786khan@gmail.com>
2021-02-21 20:04:45 +0530

    Better LSP Setup by setting shortcuts after capablities

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -1,55 +1,59 @@
-local lsp = require("lspconfig")
+local nvim_lsp = require("lspconfig")
 
-local map = function(type, key, value)
-	vim.fn.nvim_buf_set_keymap(0,type,key,value,{noremap = true, silent = true});
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  require('completion').on_attach(client)
+  print('LSP enabled')
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+  buf_set_keymap('n', '<leader>gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', '<leader>gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<leader>gS', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<leader>gr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<leader>gR', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<leader>n', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', '<leader>N', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<leader>af', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  -- buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  -- buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  -- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  -- buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  -- buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+
+  -- Set some keybinds conditional on server capabilities
+  if client.resolved_capabilities.document_formatting then
+    buf_set_keymap("n", "<leader>=", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  elseif client.resolved_capabilities.document_range_formatting then
+    buf_set_keymap("n", "<leader>=", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+  end
+
+  -- Set autocommands conditional on server_capabilities
+  -- if client.resolved_capabilities.document_highlight then
+  --   vim.api.nvim_exec([[
+  --     hi LspReferenceRead cterm=bold ctermbg=yellow guibg=blue
+  --     hi LspReferenceText cterm=bold ctermbg=yellow guibg=blue
+  --     hi LspReferenceWrite cterm=bold ctermbg=yellow guibg=blue
+  --     augroup lsp_document_highlight
+  --       autocmd! * <buffer>
+  --       autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+  --       autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+  --     augroup END
+  --   ]], false)
+  -- end
 end
 
-local custom_attach = function(client)
-	print("Language Server has been attached");
-	require'completion'.on_attach(client)
-
-	map('n','<leader>gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
-	map('n','<leader>gd','<cmd>lua vim.lsp.buf.definition()<CR>')
-	-- map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
-	map('n','<leader>gR','<cmd>lua vim.lsp.buf.references()<CR>')
-	map('n','<leader>gS','<cmd>lua vim.lsp.buf.signature_help()<CR>')
-	-- map('n','<leader>gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
-	map('n','<leader>gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
-	map('n','<leader>n', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
-	map('n','<leader>N', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
-	map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
-	map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
-	map('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
-	-- map('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
-	map('n','<leader>gr','<cmd>lua vim.lsp.buf.rename()<CR>')
-	map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-	-- map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
-	-- map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
+-- Use a loop to conveniently both setup defined servers
+-- and map buffer local keybindings when the language server attaches
+local servers = { "clangd", "cmake", "pyls", "rls", "vimls"}
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup { on_attach = on_attach }
 end
-
-lsp.clangd.setup{on_attach=custom_attach}
-lsp.cmake.setup{on_attach=custom_attach}
-lsp.pyls.setup{on_attach=custom_attach}
-lsp.rls.setup{on_attach=custom_attach}
-lsp.vimls.setup{on_attach=custom_attach}
-lsp.sumneko_lua.setup{
-	on_attach=custom_attach,
-	settings = {
-		Lua = {
-			runtime = { version = "LuaJIT", path = vim.split(package.path, ';'), },
-			completion = { keywordSnippet = "Disable", },
-			diagnostics = { enable = true, globals = {
-				"vim", "describe", "it", "before_each", "after_each" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-				}
-			}
-		}
-	}
-}
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
